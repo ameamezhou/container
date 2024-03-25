8941f9d833cc8a3112e41e2f73d654cb5f9a658e
ameamezhou <wxz17352825505@163.com>
2024-03-25 16:00:29 +0800

    k8s start

--- full.cpp
+++ full.cpp
@@ -22,3 +22,12 @@ int main()
 
     return 0;
 }
+
+SELECT e.employee_id, e.last_name,
+    CASE
+        WHEN d.department_id = 1800 THEN 'Canada'
+        ELSE 'USA'
+    END AS location
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id;
